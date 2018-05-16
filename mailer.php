6641d82bfbbe57592ee0ca34b3dbe217bbe9c1f3
scott truitt <scott@abbys-air.attlocal.net>
2018-05-15 22:24:08 -0700

    Changed the email to go to Diana

--- mailer.php
+++ mailer.php
@@ -16,7 +16,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n  \"personalizations\": [\n    {\n      \"to\": [\n        {\n          \"email\": \"scottruitt@gmail.com\"\n        }\n      ],\n      \"subject\": \"New kitten request from velvacattery.com\"\n    }\n  ],\n  \"from\": {\n    \"email\": \"velva@velvacattery.com\"\n  },\n  \"content\": [\n    {\n      \"type\": \"text/html\",\n      \"value\": \"Name: $name<br />Email: $email<br />Phone: $phone<br />Address: $address<br />City: $city<br />State: $state<br />Zip: $zip<br /></br>Comments: $comments\"\n    }\n  ]\n}",
+  CURLOPT_POSTFIELDS => "{\n  \"personalizations\": [\n    {\n      \"to\": [\n        {\n          \"email\": \"ddoernberg@aol.com\"\n        }\n      ],\n      \"subject\": \"New kitten request from velvacattery.com\"\n    }\n  ],\n  \"from\": {\n    \"email\": \"velva@velvacattery.com\"\n  },\n  \"content\": [\n    {\n      \"type\": \"text/html\",\n      \"value\": \"Name: $name<br />Email: $email<br />Phone: $phone<br />Address: $address<br />City: $city<br />State: $state<br />Zip: $zip<br /></br>Comments: $comments\"\n    }\n  ]\n}",
   CURLOPT_HTTPHEADER => array(
     "authorization: Bearer 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",
     "cache-control: no-cache",
