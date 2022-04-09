c591973c17fc8a78940f625d1273f652f2228c62
scott truitt <scottruitt@users.noreply.github.com>
2022-04-09 21:01:05 +0200

    Update contact.php
    Changed her email address in the other place too

--- contact.php
+++ contact.php
@@ -23,7 +23,7 @@
 				<h1>Contact </h1>
 				<p>Thank you for your interest in our pedigreed, CFA registered Russian Blues. </p>
 				<p>We hope this information has been helpful in your search for a new family member. If you need more information about finding a pedigreed kitten, please view the <a href="http://www.cfa.org" title="Cat Fanciers' Association website">CFA website</a>. Also, see <a href="http://www.cfainc.org/breeds/profiles/articles/russian.html" title="Diana&rsquo;s CFA Russian Blue Breed Profile">Diana&rsquo;s CFA Russian Blue Breed Profile</a>. </p>
-				<p>Velva Cattery is located in Akron, Ohio</a>. Diana can be reached via email at <a href="mailto:ddoernberg@gmail.com" title="Send an email to Diana">ddoernberg@aol.com</a>. </p>
+				<p>Velva Cattery is located in Akron, Ohio</a>. Diana can be reached via email at <a href="mailto:ddoernberg@gmail.com" title="Send an email to Diana">ddoernberg@gmail.com</a>. </p>
 
 			</div>
 	
